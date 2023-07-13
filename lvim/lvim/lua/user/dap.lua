c1e0899093cb198397f4be3faeb27d741461f2fe
Justin Stitt <justinstitt@google.com>
2023-07-13 20:25:08 +0000

    update dap.lua

--- lvim/lvim/lua/user/dap.lua
+++ lvim/lvim/lua/user/dap.lua
@@ -3,7 +3,7 @@ local dap = require('dap')
 dap.adapters.lldb = {
   type = 'executable',
   -- absolute path is important here, otherwise the argument in the `runInTerminal` request will default to $CWD/lldb-vscode
-  command = '/usr/bin/lldb-vscode-15',
+  command = '/usr/bin/lldb-vscode-15', -- can install lldb-vscode-14 with https://github.com/xbc5/DAPInstall.nvim#configuration
   name = "lldb"
 }
 
@@ -21,3 +21,21 @@ dap.configurations.cpp = {
     runInTerminal = true,
   },
 }
+
+dap.configurations.c = {
+  {
+    name = "Launch",
+    type = "lldb",
+    request = "launch",
+    program = function()
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+    cwd = '${workspaceFolder}',
+    stopOnEntry = false,
+    args = {},
+    runInTerminal = true,
+  },
+}
+
+-- run this as per: https://github.com/mfussenegger/nvim-dap/issues/210
+-- echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
