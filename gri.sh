e3560a4718f4f703ece2f9f5a4e1eece6b2f6e1b
Justin Stitt <justinstitt@google.com>
2023-08-08 21:15:27 +0000

    add b4 custom patch

--- gri.sh
+++ gri.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 #
 NUM_COMMITS=$1
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied. Need NUM_COMMITS for git rebase -i HEAD~\$NUM_COMMITS"
+    exit 1
+fi
 GIT_SEQUENCE_EDITOR=/usr/bin/rebaser git rebase -i HEAD~$NUM_COMMITS
